554c2258adc9ab05cc88d5625f5798acde4ff2b3
maxiroots-star <maxiroots@googlemail.com>
2026-06-15 19:05:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 
+const trackA = document.getElementById("trackA");
+const trackB = document.getElementById("trackB");
+
+const playA = document.getElementById("playA");
+const playB = document.getElementById("playB");
+const crossfader = document.getElementById("crossfader");
+
+/* 🔓 UNLOCK AUDIO (mobile fix) */
+function unlockAudio() {
+    trackA.volume = 0.5;
+    trackB.volume = 0.5;
+
+    trackA.play().then(() => trackA.pause()).catch(()=>{});
+    trackB.play().then(() => trackB.pause()).catch(()=>{});
+}
+
+document.addEventListener("click", unlockAudio, { once: true });
+document.addEventListener("touchstart", unlockAudio, { once: true });
+
+/* ▶ PLAY BUTTONS */
+playA.addEventListener("click", () => {
+    trackA.currentTime = 0;
+    trackA.play();
+});
+
+playB.addEventListener("click", () => {
+    trackB.currentTime = 0;
+    trackB.play();
+});
+
+/* 🎚 CROSSFADER */
+crossfader.addEventListener("input", () => {
+    const v = crossfader.value / 100;
+
+    trackA.volume = 1 - v;
+    trackB.volume = v;
+});
